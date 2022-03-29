46b5e8c92f838bf6af1515e5d0b100b86c24bc4b
Syahrizal Fauzi <44159978+syahrizalfauzi@users.noreply.github.com>
2022-03-29 17:11:25 +0700

    ItemCard done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:himaskom_undip/widgets/articlecard.dart';
-import 'package:himaskom_undip/widgets/articlelistitem.dart';
+import 'package:himaskom_undip/widgets/itemcard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -46,29 +45,49 @@ class _PageState extends State<Page> {
       ),
       body: Form(
         key: _formKey,
-        child: ListView.builder(
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 2 / 3,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+          ),
           itemCount: 10,
           itemBuilder: (context, index) {
-            if (index == 0) {
-              return ArticleCard(
-                judul:
-                    '[Jenis] Article Nomor $index Tapi Super Duper Panjang Kayak Gimana Gitu Ya SAD ASD ASD ASD ADS ASD ASD ASD ',
-                id: index.toString(),
-                createdAt: DateTime.now(),
-                thumbnailUrl:
-                    'https://upload.wikimedia.org/wikipedia/commons/e/ee/Sample_abc.jpg',
-              );
-            }
-            return ArticleListItem(
+            return ItemCard(
               judul:
                   '[Jenis] Article Nomor $index Tapi Super Duper Panjang Kayak Gimana Gitu Ya SAD ASD ASD ASD ADS ASD ASD ASD ',
               id: index.toString(),
-              createdAt: DateTime.now(),
+              harga: 25000 * (index + 1),
               thumbnailUrl:
                   'https://upload.wikimedia.org/wikipedia/commons/e/ee/Sample_abc.jpg',
             );
           },
         ),
+        // ListView.builder(
+        //   itemCount: 10,
+        //   itemBuilder: (context, index) {
+        //     if (index == 0) {
+        //       return ArticleCard(
+        //         judul:
+        //             '[Jenis] Article Nomor $index Tapi Super Duper Panjang Kayak Gimana Gitu Ya SAD ASD ASD ASD ADS ASD ASD ASD ',
+        //         id: index.toString(),
+        //         createdAt: DateTime.now(),
+        //         thumbnailUrl:
+        //             'https://upload.wikimedia.org/wikipedia/commons/e/ee/Sample_abc.jpg',
+        //       );
+        //     }
+        //     return ArticleListItem(
+        //       judul:
+        //           '[Jenis] Article Nomor $index Tapi Super Duper Panjang Kayak Gimana Gitu Ya SAD ASD ASD ASD ADS ASD ASD ASD ',
+        //       id: index.toString(),
+        //       createdAt: DateTime.now(),
+        //       thumbnailUrl:
+        //           'https://upload.wikimedia.org/wikipedia/commons/e/ee/Sample_abc.jpg',
+        //     );
+        //   },
+        // ),
+        //////////////////////////
         // Center(
         //   child: Column(
         //     mainAxisAlignment: MainAxisAlignment.center,
---
+++ lib/widgets/itemcard.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class ItemCard extends StatelessWidget {
+  final String judul;
+  final String id;
+  final int harga;
+  final String thumbnailUrl;
+  const ItemCard({
+    Key? key,
+    required this.judul,
+    required this.id,
+    required this.harga,
+    required this.thumbnailUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      shadowColor: const Color(0x328D92AB),
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.network(thumbnailUrl, fit: BoxFit.cover),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                judul,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                style: const TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                  height: 1.5,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                NumberFormat.currency(
+                  locale: 'id_ID',
+                  symbol: "Rp. ",
+                  decimalDigits: 0,
+                ).format(harga),
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xFFB4B3BA),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
