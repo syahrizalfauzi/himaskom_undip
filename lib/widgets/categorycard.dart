3c7a8be83740cff2b0802abc038b715ac737248f
Syahrizal Fauzi <44159978+syahrizalfauzi@users.noreply.github.com>
2022-03-30 14:18:29 +0700

    Added flutter_svg, CategoryCard done

---
+++ lib/widgets/categorycard.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class CategoryCard extends StatelessWidget {
+  final String title;
+  final String svgAsset;
+  final String subtitle;
+  final void Function() onTap;
+  const CategoryCard({
+    Key? key,
+    required this.title,
+    required this.svgAsset,
+    required this.subtitle,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      shadowColor: const Color(0x328D92AB),
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SvgPicture.asset(svgAsset, height: 40, width: 40),
+                  const SizedBox(width: 12),
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 16),
+              Text(
+                subtitle,
+                style: const TextStyle(
+                  fontSize: 12,
+                  height: 1.6,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFF595A6B),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
