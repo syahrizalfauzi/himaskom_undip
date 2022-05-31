e1e593600e97f0ab8932084220388548c626e478
Syahrizal Fauzi <44159978+syahrizalfauzi@users.noreply.github.com>
2022-05-31 09:56:17 +0700

    Finishing

--- lib/pages/article_editor_pres.dart
+++ lib/pages/article_editor_pres.dart
@@ -55,139 +55,142 @@ class ArticleEditorPagePresentational extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-            children: [
-              Text(
-                'Tambahkan Foto',
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              const SizedBox(height: 16),
-              AbsorbPointer(
-                absorbing: isLoading,
-                child: ImagePicker(
-                  onChange: onImageChange,
-                  onRemove: onImageRemove,
-                  initialImageUrls: initialImageUrls,
-                  isError: isImageError,
-                ),
-              ),
-              if (isImageError) ...[
-                const SizedBox(height: 8),
+    return WillPopScope(
+      onWillPop: () async => !isLoading,
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+              children: [
                 Text(
-                  'Harus diisi',
-                  style: TextStyle(color: Theme.of(context).errorColor),
-                ),
-              ],
-              const SizedBox(height: 24),
-              CustomTextFormField(
-                disabled: isLoading,
-                useBorder: false,
-                labelText: "Tambahkan Judul",
-                tipText: "Max. 20 Karakter",
-                hintText: "Masukkan Judul",
-                textInputAction: TextInputAction.next,
-                controller: judulController,
-                minLength: 5,
-                maxLength: 20,
-                onSubmit: onJudulSubmit,
-              ),
-              if (tenggatDateController != null &&
-                  onTenggatDateTap != null &&
-                  tenggatTimeController != null &&
-                  onTenggatTimeTap != null) ...[
-                const SizedBox(height: 24),
-                Text(
-                  'Tambahkan Tenggat',
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                const SizedBox(height: 18),
-                Row(
-                  children: [
-                    Expanded(
-                      child: CustomTextFormField(
-                        disabled: isLoading,
-                        useBorder: false,
-                        hintText: "DD/MM/YYYY",
-                        suffixIcon: const Icon(Icons.calendar_month_outlined),
-                        controller: tenggatDateController,
-                        onTap: onTenggatDateTap,
-                        validator: tenggatDateValidator,
-                      ),
-                    ),
-                    const SizedBox(width: 18),
-                    Expanded(
-                      child: CustomTextFormField(
-                          disabled: isLoading,
-                          useBorder: false,
-                          hintText: "HH:MM",
-                          suffixIcon: const Icon(Icons.watch_later_outlined),
-                          controller: tenggatTimeController,
-                          onTap: onTenggatTimeTap,
-                          validator: tenggatTimeValidator),
-                    ),
-                  ],
-                ),
-              ],
-              if (tags != null &&
-                  onTagChange != null &&
-                  selectedTagIndex != null) ...[
-                const SizedBox(height: 24),
-                Text(
-                  'Pilih Tag',
+                  'Tambahkan Foto',
                   style: Theme.of(context).textTheme.bodyLarge,
                 ),
                 const SizedBox(height: 16),
                 AbsorbPointer(
                   absorbing: isLoading,
-                  child: TagPicker(
-                    tags: tags!,
-                    onTap: onTagChange!,
-                    selectedIndex: selectedTagIndex!,
+                  child: ImagePicker(
+                    onChange: onImageChange,
+                    onRemove: onImageRemove,
+                    initialImageUrls: initialImageUrls,
+                    isError: isImageError,
                   ),
                 ),
-              ],
-              const SizedBox(height: 24),
-              CustomTextFormField(
-                disabled: isLoading,
-                useBorder: false,
-                labelText: "Tambahkan Deskripsi",
-                tipText: "Min. 20 Karakter",
-                multiline: true,
-                hintText: "Masukkan Deskripsi",
-                controller: deskripsiController,
-                minLength: 20,
-                focusNode: deskripsiNode,
-              ),
-              if (hargaController != null) ...[
+                if (isImageError) ...[
+                  const SizedBox(height: 8),
+                  Text(
+                    'Harus diisi',
+                    style: TextStyle(color: Theme.of(context).errorColor),
+                  ),
+                ],
                 const SizedBox(height: 24),
                 CustomTextFormField(
-                  validator: hargaValidator,
                   disabled: isLoading,
                   useBorder: false,
-                  labelText: "Tambahkan Harga",
-                  hintText: "Masukkan Harga (IDR)",
-                  textInputAction: TextInputAction.done,
-                  textInputType: TextInputType.number,
-                  controller: hargaController,
+                  labelText: "Tambahkan Judul",
+                  tipText: "Max. 20 Karakter",
+                  hintText: "Masukkan Judul",
+                  textInputAction: TextInputAction.next,
+                  controller: judulController,
+                  minLength: 5,
+                  maxLength: 20,
+                  onSubmit: onJudulSubmit,
                 ),
+                if (tenggatDateController != null &&
+                    onTenggatDateTap != null &&
+                    tenggatTimeController != null &&
+                    onTenggatTimeTap != null) ...[
+                  const SizedBox(height: 24),
+                  Text(
+                    'Tambahkan Tenggat',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  const SizedBox(height: 18),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: CustomTextFormField(
+                          disabled: isLoading,
+                          useBorder: false,
+                          hintText: "DD/MM/YYYY",
+                          suffixIcon: const Icon(Icons.calendar_month_outlined),
+                          controller: tenggatDateController,
+                          onTap: onTenggatDateTap,
+                          validator: tenggatDateValidator,
+                        ),
+                      ),
+                      const SizedBox(width: 18),
+                      Expanded(
+                        child: CustomTextFormField(
+                            disabled: isLoading,
+                            useBorder: false,
+                            hintText: "HH:MM",
+                            suffixIcon: const Icon(Icons.watch_later_outlined),
+                            controller: tenggatTimeController,
+                            onTap: onTenggatTimeTap,
+                            validator: tenggatTimeValidator),
+                      ),
+                    ],
+                  ),
+                ],
+                if (tags != null &&
+                    onTagChange != null &&
+                    selectedTagIndex != null) ...[
+                  const SizedBox(height: 24),
+                  Text(
+                    'Pilih Tag',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  const SizedBox(height: 16),
+                  AbsorbPointer(
+                    absorbing: isLoading,
+                    child: TagPicker(
+                      tags: tags!,
+                      onTap: onTagChange!,
+                      selectedIndex: selectedTagIndex!,
+                    ),
+                  ),
+                ],
+                const SizedBox(height: 24),
+                CustomTextFormField(
+                  disabled: isLoading,
+                  useBorder: false,
+                  labelText: "Tambahkan Deskripsi",
+                  tipText: "Min. 20 Karakter",
+                  multiline: true,
+                  hintText: "Masukkan Deskripsi",
+                  controller: deskripsiController,
+                  minLength: 20,
+                  focusNode: deskripsiNode,
+                ),
+                if (hargaController != null) ...[
+                  const SizedBox(height: 24),
+                  CustomTextFormField(
+                    validator: hargaValidator,
+                    disabled: isLoading,
+                    useBorder: false,
+                    labelText: "Tambahkan Harga",
+                    hintText: "Masukkan Harga (IDR)",
+                    textInputAction: TextInputAction.done,
+                    textInputType: TextInputType.number,
+                    controller: hargaController,
+                  ),
+                ],
               ],
-            ],
+            ),
           ),
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(24, 12, 24, 32),
-          width: double.infinity,
-          child: CustomButton(
-            onTap: onSubmit,
-            text: "Selesai",
-            loading: isLoading,
-          ),
-        )
-      ],
+          Container(
+            padding: const EdgeInsets.fromLTRB(24, 12, 24, 32),
+            width: double.infinity,
+            child: CustomButton(
+              onTap: onSubmit,
+              text: "Selesai",
+              loading: isLoading,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
