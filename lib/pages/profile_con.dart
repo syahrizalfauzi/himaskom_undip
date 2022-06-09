cdf5741539d5532a3ed4fdd309cc520712eebd27
Syahrizal Fauzi <44159978+syahrizalfauzi@users.noreply.github.com>
2022-06-09 17:35:09 +0700

    Changed profile page container to be stateless

--- lib/pages/profile_con.dart
+++ lib/pages/profile_con.dart
@@ -1,14 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:himaskom_undip/pages/profile_pres.dart';
 
-class ProfilePageContainer extends HookWidget {
+class ProfilePageContainer extends StatelessWidget {
   const ProfilePageContainer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final _user = useMemoized(() => FirebaseAuth.instance.currentUser!, []);
+    final _user = FirebaseAuth.instance.currentUser!;
 
     return ProfilePagePresentational(
       email: _user.email!,
