b916173980c3f8cac43d55bffc618de604d7c7f7
Syahrizal Fauzi <44159978+syahrizalfauzi@users.noreply.github.com>
2022-06-30 05:57:17 +0700

    Refactored admin home to be in admin

--- lib/pages/admin_con.dart
+++ lib/pages/admin_con.dart
@@ -5,7 +5,9 @@ import 'package:himaskom_undip/models/article.dart';
 import 'package:himaskom_undip/models/article_state_item.dart';
 import 'package:himaskom_undip/pages/admin_pres.dart';
 import 'package:himaskom_undip/pages/article_editor_con.dart';
+import 'package:himaskom_undip/providers/article_states.dart';
 import 'package:himaskom_undip/utils/get_article_state.dart';
+import 'package:himaskom_undip/utils/push_article_state_page.dart';
 import 'package:himaskom_undip/widgets/admin_scaffold.dart';
 import 'package:himaskom_undip/widgets/custom_snackbar.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -15,6 +17,37 @@ class AdminContainer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
+    final _akademikState = ref.watch(akademikArticleState);
+    final _beasiswaState = ref.watch(beasiswaArticleState);
+    final _prestasiState = ref.watch(prestasiArticleState);
+    final _sistoreState = ref.watch(sistoreArticleState);
+    final _umumState = ref.watch(umumArticleState);
+    final _eventStates = [
+      ref.watch(eventAmArticleState),
+      ref.watch(eventHmArticleState),
+      ref.watch(eventUkmArticleState),
+    ];
+    final _karirStates = [
+      ref.watch(karirLokerArticleState),
+      ref.watch(karirMagangArticleState),
+    ];
+    final _lombaStates = [
+      ref.watch(lombaAkademikArticleState),
+      ref.watch(lombaNonakademikArticleState),
+    ];
+    _handleLogOut() => FirebaseAuth.instance.signOut();
+
+    _handleTapAddArticle(ArticleStateItem stateItem) {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => AdminScaffold(
+            title: stateItem.title,
+            child: ArticleEditorPageContainer(stateItem: stateItem),
+          ),
+        ),
+      );
+    }
+
     _handleTapArticle(Article article) {
       final articleState = ref.read(getArticleStateFromArticle(article));
       final stateItem = ArticleStateItem.fromArticleState(
@@ -85,9 +118,205 @@ class AdminContainer extends ConsumerWidget {
       );
     }
 
+    _handleTapEvent() {
+      for (final e in _eventStates) {
+        e.getAll(false);
+      }
+      pushArticleStatesPage(
+        context: context,
+        states: [
+          eventAmArticleState,
+          eventHmArticleState,
+          eventUkmArticleState
+        ],
+        builder: (states) => AdminScaffold.withArticleShortList(
+          stateItem: ArticleStateItem.fromArticleStates(
+            states: states,
+            title: "Event",
+          ),
+          onTapAddArticle: _handleTapAddArticle,
+          onTapArticle: _handleTapArticle,
+          onDeleteArticle: _handleDeleteArticle,
+        ),
+      );
+    }
+
+    _handleTapSistore() {
+      _sistoreState.getAll(false);
+      pushArticleStatesPage(
+        context: context,
+        states: [sistoreArticleState],
+        builder: (states) => AdminScaffold.withArticleShortList(
+          stateItem: ArticleStateItem.fromArticleStates(
+            states: states,
+            title: "Sistore",
+          ),
+          onTapAddArticle: _handleTapAddArticle,
+          onTapArticle: _handleTapArticle,
+          onDeleteArticle: _handleDeleteArticle,
+        ),
+      );
+    }
+
+    _handleTapBeasiswa() {
+      _beasiswaState.getAll(false);
+      pushArticleStatesPage(
+        context: context,
+        states: [beasiswaArticleState],
+        builder: (states) => AdminScaffold.withArticleShortList(
+          stateItem: ArticleStateItem.fromArticleStates(
+            states: states,
+            title: "Beasiswa",
+          ),
+          onTapAddArticle: _handleTapAddArticle,
+          onTapArticle: _handleTapArticle,
+          onDeleteArticle: _handleDeleteArticle,
+        ),
+      );
+    }
+
+    _handleTapPrestasi() {
+      _prestasiState.getAll(false);
+      pushArticleStatesPage(
+        context: context,
+        states: [prestasiArticleState],
+        builder: (states) => AdminScaffold.withArticleShortList(
+          stateItem: ArticleStateItem.fromArticleStates(
+            states: states,
+            title: "Prestasi",
+          ),
+          onTapAddArticle: _handleTapAddArticle,
+          onTapArticle: _handleTapArticle,
+          onDeleteArticle: _handleDeleteArticle,
+        ),
+      );
+    }
+
+    _handleTapAkademik() {
+      _akademikState.getAll(false);
+      pushArticleStatesPage(
+        context: context,
+        states: [akademikArticleState],
+        builder: (states) => AdminScaffold.withArticleShortList(
+          stateItem: ArticleStateItem.fromArticleStates(
+            states: states,
+            title: "Akademik",
+          ),
+          onTapAddArticle: _handleTapAddArticle,
+          onTapArticle: _handleTapArticle,
+          onDeleteArticle: _handleDeleteArticle,
+        ),
+      );
+    }
+
+    _handleTapKarir() {
+      for (final e in _karirStates) {
+        e.getAll(false);
+      }
+      pushArticleStatesPage(
+        context: context,
+        states: [karirLokerArticleState, karirMagangArticleState],
+        builder: (states) => AdminScaffold.withArticleShortList(
+          stateItem: ArticleStateItem.fromArticleStates(
+            states: states,
+            title: "Karir",
+          ),
+          onTapAddArticle: _handleTapAddArticle,
+          onTapArticle: _handleTapArticle,
+          onDeleteArticle: _handleDeleteArticle,
+        ),
+      );
+    }
+
+    _handleTapLomba() {
+      for (final e in _lombaStates) {
+        e.getAll(false);
+      }
+      pushArticleStatesPage(
+        context: context,
+        states: [lombaAkademikArticleState, lombaNonakademikArticleState],
+        builder: (states) => AdminScaffold.withArticleShortList(
+          stateItem: ArticleStateItem.fromArticleStates(
+            states: states,
+            title: "Lomba",
+          ),
+          onTapAddArticle: _handleTapAddArticle,
+          onTapArticle: _handleTapArticle,
+          onDeleteArticle: _handleDeleteArticle,
+        ),
+      );
+    }
+
+    _handleTapUmum() {
+      _umumState.getAll(false);
+      pushArticleStatesPage(
+        context: context,
+        states: [umumArticleState],
+        builder: (states) => AdminScaffold.withArticleShortList(
+          stateItem: ArticleStateItem.fromArticleStates(
+            states: states,
+            title: "Umum",
+          ),
+          onTapAddArticle: _handleTapAddArticle,
+          onTapArticle: _handleTapArticle,
+          onDeleteArticle: _handleDeleteArticle,
+        ),
+      );
+    }
+
+    // return AdminPresentational(
+    //   onTapArticle: _handleTapArticle,
+    //   onDeleteArticle: _handleDeleteArticle,
+    // );
+
+    final items = [
+      ArticleStateItem.fromArticleStates(
+        states: _eventStates,
+        title: 'Event',
+        onTap: _handleTapEvent,
+      ),
+      ArticleStateItem.fromArticleState(
+        state: _sistoreState,
+        onTap: _handleTapSistore,
+        onRefresh: _sistoreState.getAll,
+      ),
+      ArticleStateItem.fromArticleState(
+        state: _beasiswaState,
+        onTap: _handleTapBeasiswa,
+        onRefresh: _beasiswaState.getAll,
+      ),
+      ArticleStateItem.fromArticleState(
+        state: _prestasiState,
+        onTap: _handleTapPrestasi,
+        onRefresh: _prestasiState.getAll,
+      ),
+      ArticleStateItem.fromArticleState(
+        state: _akademikState,
+        onTap: _handleTapAkademik,
+        onRefresh: _akademikState.getAll,
+      ),
+      ArticleStateItem.fromArticleStates(
+        states: _karirStates,
+        title: 'Karir',
+        onTap: _handleTapKarir,
+      ),
+      ArticleStateItem.fromArticleStates(
+        states: _lombaStates,
+        title: 'Lomba',
+        onTap: _handleTapLomba,
+      ),
+      ArticleStateItem.fromArticleState(
+        state: _umumState,
+        onTap: _handleTapUmum,
+        onRefresh: _umumState.getAll,
+      ),
+    ];
+
     return AdminPresentational(
       onTapArticle: _handleTapArticle,
       onDeleteArticle: _handleDeleteArticle,
+      onLogOut: _handleLogOut,
+      items: items,
     );
   }
 }
--- lib/pages/admin_pres.dart
+++ lib/pages/admin_pres.dart
@@ -1,25 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:himaskom_undip/models/article.dart';
-import 'package:himaskom_undip/pages/admin_home_con.dart';
+import 'package:himaskom_undip/models/article_state_item.dart';
 import 'package:himaskom_undip/widgets/admin_scaffold.dart';
 
 class AdminPresentational extends StatelessWidget {
   final Function(Article) onTapArticle;
   final Function(Article) onDeleteArticle;
+  final Function() onLogOut;
+  final List<ArticleStateItem> items;
 
   const AdminPresentational({
     Key? key,
     required this.onTapArticle,
     required this.onDeleteArticle,
+    required this.onLogOut,
+    required this.items,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // return AdminScaffold(
+    //   title: 'Home',
+    //   child: AdminHomePageContainer(
+    //     onTapArticle: onTapArticle,
+    //     onDeleteArticle: onDeleteArticle,
+    //   ),
+    // );
     return AdminScaffold(
       title: 'Home',
-      child: AdminHomePageContainer(
-        onTapArticle: onTapArticle,
-        onDeleteArticle: onDeleteArticle,
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.only(
+                    top: 16,
+                    left: 24,
+                    right: 24,
+                    bottom: index == 9 ? 16 : 0,
+                  ),
+                  child: Card(
+                    elevation: 10,
+                    shadowColor: const Color(0x328D92AB),
+                    child: InkWell(
+                      onTap: items[index].onTap,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                items[index].title,
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                            ),
+                            const Icon(Icons.keyboard_arrow_right),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(48),
+            child: SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: onLogOut,
+                child: Text(
+                  'Log Out',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .copyWith(color: Colors.red),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
